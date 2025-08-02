83b7a04ebdd40fab0f9d0d4f1ea72c16e3c96009
David Snyder <37163053+AlwaysLearningTech@users.noreply.github.com>
2025-08-01 23:03:40 -0700

    Update generate.py

--- input/default/generate.py
+++ input/default/generate.py
@@ -24,7 +24,7 @@ CodeplugRecipe(
     order=cp_dir / "order.csv",
     replacements=cp_dir / "replacements.csv",
     output_anytone=True,
-    output_dmrconfig=[(cp_dir / "gd-77-default.conf")],
+    output_dmrconfig=[(cp_dir / "gd77-default.conf")],
     output_farnsworth=[(cp_dir / "example-md-uv380.json")],
     output_gb3gf=True
 ).generate(output / cp_dir.name)
