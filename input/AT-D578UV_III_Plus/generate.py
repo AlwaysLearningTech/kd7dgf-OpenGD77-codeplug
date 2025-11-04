a7ead4bd24a54a2af8c7c522bb53432abb6f1063
David Snyder <snyder.dl@outlook.com>
2025-11-03 18:40:12 -0800

    Add new channel configurations and related data files for AT-D878UV II Plus
    - Created Analog__W7ACS_20250917.csv with extensive channel data for ACS - VHF and UHF bands.
    - Added Analog__WA_Beachnet_Repeater_System_Channels.csv for Beachnet repeater system channels.
    - Initialized order.csv to define the order of zones, contacts, and channels.
    - Added prox.csv containing geographical data for various zones.
    - Created replacements.csv for channel and zone name replacements.
    - Initialized scanlists.json as an empty configuration file for future use.

---
+++ input/AT-D578UV_III_Plus/generate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# This generates the same codeplug as generate.sh
+# using python code.
+
+from pathlib import Path
+import os
+
+from dzcb.recipe import CodeplugRecipe
+
+cp_dir = Path(__file__).parent
+output = Path(os.environ.get("OUTPUT") or (cp_dir / ".." / ".." / "OUTPUT"))
+
+CodeplugRecipe(
+    source_pnwdigital=True,
+    source_seattledmr=True,
+    source_default_k7abd=False,
+    source_k7abd=[(cp_dir / "k7abd")],
+    source_repeaterbook_proximity=cp_dir / "prox.csv",
+    repeaterbook_states=["washington", "oregon"],
+    repeaterbook_name_format='{Callsign} {Nearest City} {Landmark}',
+    scanlists_json=cp_dir / "scanlists.json",
+    exclude=cp_dir / "exclude.csv",
+    order=cp_dir / "order.csv",
+    replacements=cp_dir / "replacements.csv",
+    output_anytone=True,
+    output_dmrconfig=[(cp_dir / "d578uv-default.conf")],
+    output_farnsworth=False,
+    output_gb3gf=False
+).generate(output / cp_dir.name)
